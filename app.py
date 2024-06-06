053b031c30e7bf8bf7cb88d429abaa81b99f5a52
YuRouLin <168400039+YuRouLin@users.noreply.github.com>
2024-06-06 23:57:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -8,41 +7,14 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-import traceback
-#======python的函數庫==========
-
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
 # Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
 # Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    questions_answers = {
+questions_answers = {
         '台數': {
             "莊家": "1台",
             "莊連一": "3台",
@@ -124,9 +96,21 @@ def handle_message(event):
             "槓上開花": "因家槓、暗槓、摸到花牌而補牌，補牌後又剛好胡牌。",
             "搶槓胡": "當對手加槓時，加槓的那一張牌剛好是胡牌者所要胡的牌，胡牌者可以選擇搶槓胡牌，視為加槓者放槍。",
         }
-    }
-    
-    
+}
+
+user_state = {}
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
@@ -151,25 +135,5 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(event.reply_token, TextSendMessage("未找到相關答案，請重新輸入相對應的關鍵字"))
 
-       
-         
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
-        
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
